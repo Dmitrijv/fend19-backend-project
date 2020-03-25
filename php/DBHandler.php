57f39dfe777887bf0b5efa2bb9109364bbe71022
Dmitrijv <dmitrij.vel@live.se>
2020-03-25 16:46:15 +0100

    small refactor

--- php/DBHandler.php
+++ php/DBHandler.php
@@ -12,7 +12,7 @@ class DBHandler {
 	public function connect() {
 		$this->conn = mysqli_connect($this->server, $this->username, $this->password, $this->database);
 		if ($this->conn) {
-			echo 'Connected successfully!';
+			//echo 'Connected successfully!';
 		} else {
 			die("Connection failed: " . mysqli_connect_error());
 		}
@@ -28,28 +28,11 @@ class DBHandler {
 		return $posts;
 	}
 
-	// private $con;
-
-	// public function connect() {
-
-	// 	$this->connection = mysqli_connect($this->DB_HOST, $this->DB_USER, $this->DB_PASS, $this->DB_NAME);
-
-	// 	if (!$this->con) {
-	// 		echo "[DB Handler] Error: Unable to connect to MySQL." . PHP_EOL;
-	// 		echo "[DB Handler] Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-	// 		echo "[DB Handler] Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	// 		exit;
-	// 	}
-				
-	// 	echo ("[DB Handler] Connected to MySQL host " . mysqli_get_host_info($this->con) . PHP_EOL);
-    // }
-
     public function closeConnection(){
         mysqli_close($this->conn);
         unset($this->conn);
-        echo ("[DB Handler] Closed a connection." . PHP_EOL);
+        //echo ("[DB Handler] Closed a connection." . PHP_EOL);
     }
-
 
 }
 
