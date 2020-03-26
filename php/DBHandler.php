cb63d41301559943f38e5bf13795111f171a02a6
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-03-27 00:12:00 +0100

    added deletePost and editPost function

--- php/DBHandler.php
+++ php/DBHandler.php
@@ -1,6 +1,7 @@
 <?php
 
-class DBHandler {
+class DBHandler
+{
 
 	private $servername = "localhost";
 	private $username = "root";
@@ -10,7 +11,8 @@ class DBHandler {
 	private $conn;
 	private $pdoConn;
 
-	public function connect() {
+	public function connect()
+	{
 		$this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
 		if ($this->conn) {
 			//echo 'Connected successfully!';
@@ -18,19 +20,21 @@ class DBHandler {
 			die("Connection failed: " . mysqli_connect_error());
 		}
 	}
-	
-	public function connectPdo(){
+
+	public function connectPdo()
+	{
 		try {
 			$servername = $this->servername;
 			$database = $this->database;
 			$this->pdoConn = new PDO("mysql:host=$servername;dbname=$database", $this->username, $this->password);
 			$this->pdoConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch(PDOException $e) {
-			die ("Connection failed: " . $e->getMessage());
-		}		
+		} catch (PDOException $e) {
+			die("Connection failed: " . $e->getMessage());
+		}
 	}
 
-	public function createNewBlogPost($post){
+	public function createNewBlogPost($post)
+	{
 
 		$title = $post["title"];
 		$body = $post["body"];
@@ -41,40 +45,60 @@ class DBHandler {
 			$sql = "INSERT INTO post (id, published, title, body, date_created, date_last_edit)
 			VALUES (NULL, '$published', '$title', '$body', '$dateCreated', NULL)";
 			$this->pdoConn->exec($sql);
-		} catch(PDOException $e) {
+		} catch (PDOException $e) {
 			echo $sql . "<br>" . $e->getMessage();
 		}
-
 	}
 
-	public function getBlogPosts() {
+	public function getBlogPosts()
+	{
 		$query = 'SELECT * FROM post WHERE published = TRUE ORDER BY date_created DESC';
 		$queryResult = mysqli_query($this->conn, $query);
 		$posts = [];
-		while($tableRow = mysqli_fetch_assoc($queryResult)) {
+		while ($tableRow = mysqli_fetch_assoc($queryResult)) {
 			array_push($posts, $tableRow);
 		}
 		return $posts;
 	}
 
-	public function showAllPosts(){
+	public function showAllPosts()
+	{
 		$query = 'SELECT * FROM post ORDER BY date_created DESC';
 		$queryResult = mysqli_query($this->conn, $query);
 		$posts = [];
-		while($tableRow = mysqli_fetch_assoc($queryResult)) {
+		while ($tableRow = mysqli_fetch_assoc($queryResult)) {
 			array_push($posts, $tableRow);
 		}
 		return $posts;
 	}
 
-    public function closeConnection(){
-        mysqli_close($this->conn);
-		unset($this->conn);
-        //echo ("[DB Handler] Closed a connection." . PHP_EOL);
-	}
-	
-	public function closePdoConnection(){
-		$this->pdoConn = null;
+	public function deletePost($delete_id)
+	{
+		$query = "DELETE FROM post WHERE id = '$delete_id'";
+		$queryResult = mysqli_query($this->conn, $query);
+		if ($queryResult) {
+			header('Location: index.php');
+		} else {
+			echo 'ERROR: ' . mysqli_error($this->conn);
+		}
 	}
 
+	public function editPost($edit_id)
+	{
+		$query = "SELECT * FROM post WHERE id = '$edit_id'";
+		$queryResult =  mysqli_query($this->conn, $query);
+		return mysqli_fetch_assoc($queryResult);
+	}
+
+	public function closeConnection()
+	{
+		mysqli_close($this->conn);
+		unset($this->conn);
+		//echo ("[DB Handler] Closed a connection." . PHP_EOL);
+	}
+
+	public function closePdoConnection()
+	{
+		$this->pdoConn = null;
+	}
 }
