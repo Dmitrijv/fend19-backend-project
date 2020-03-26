d49fdfaed47ee4e99552b403de1b35a59913aed8
Dmitrijv <dmitrij.vel@live.se>
2020-03-26 11:02:00 +0100

    misc style changes to admin pages

--- admin/edit.php
+++ admin/edit.php
@@ -4,76 +4,42 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin Area | Edit Page</title>
+    <title>Admin Area | Edit Post</title>
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
     <script src="http://cdn.ckeditor.com/4.6.1/standard/ckeditor.js"></script>
+    <!-- Custom CSS -->
+    <link href="../css/customStyle.css" rel="stylesheet">      
   </head>
   <body>
 
-    <header id="header">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-10">
-            <h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Edit Page<small>About</small></h1>
-          </div>
-          <div class="col-md-2">
-            <div class="dropdown create">
-              <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                Create Content
-                <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                <li><a type="button" data-toggle="modal" data-target="#addPage">Add Page</a></li>
-                <li><a href="#">Add Post</a></li>
-                <li><a href="#">Add User</a></li>
-              </ul>
-            </div>
-          </div>
+
+  <header id="header" class="public-header">
+    <div class="container">
+
+        <div class="col-md-10">
+            <h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Edit Post <small>About</small></h1>
         </div>
-      </div>
+
+    </div>
     </header>
 
     <section id="breadcrumb">
       <div class="container">
         <ol class="breadcrumb">
           <li><a href="index.php">Dashboard</a></li>
-          <li><a href="pages.html">Pages</a></li>
-          <li class="active">Edit Page</li>
+          <li class="active">Edit Post</li>
         </ol>
       </div>
     </section>
 
     <section id="main">
       <div class="container">
-        <div class="row">
-          <div class="col-md-3">
-            <div class="list-group">
-              <a href="index.php" class="list-group-item active main-color-bg">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Dashboard
-              </a>
-              <a href="pages.html" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Pages <span class="badge">12</span></a>
-              <a href="posts.html" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Posts <span class="badge">33</span></a>
-              <a href="users.html" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Users <span class="badge">203</span></a>
-            </div>
+        <div class="">
 
-            <div class="well">
-              <h4>Disk Space Used</h4>
-              <div class="progress">
-                  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                      60%
-              </div>
-            </div>
-            <h4>Bandwidth Used </h4>
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-                    40%
-            </div>
-          </div>
-            </div>
-          </div>
-          <div class="col-md-9">
+
+          <div class="">
             <!-- Website Overview -->
             <div class="panel panel-default">
               <div class="panel-heading main-color-bg">
@@ -96,14 +62,6 @@
                       <input type="checkbox" checked> Published
                     </label>
                   </div>
-                  <div class="form-group">
-                    <label>Meta Tags</label>
-                    <input type="text" class="form-control" placeholder="Add Some Tags..." value="tag1, tag2">
-                  </div>
-                  <div class="form-group">
-                    <label>Meta Description</label>
-                    <input type="text" class="form-control" placeholder="Add Meta Description..." value="  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et ">
-                  </div>
                   <input type="submit" class="btn btn-default" value="Submit">
                 </form>
               </div>
@@ -114,9 +72,12 @@
       </div>
     </section>
 
-    <footer id="footer">
-      <p>Copyright AdminStrap, &copy; 2017</p>
-    </footer>
+  <footer id="footer">
+    <p>FEND19 - Backend - CMS</p>
+    <p>Dmitrij Velström, Shan Mi</p>
+    <p>Nackademin</p>
+    <p>2020-03-26</p>
+  </footer>
 
     <!-- Modals -->
 
--- index.php
+++ index.php
@@ -18,47 +18,48 @@
 
   <header id="header" class="public-header">
     <div class="container">
-      <div class="row">
+
         <div class="col-md-10">
           <h1></span> FEND19 - Backend - CMS <small>by Dmitrij Velström & Shan Mi</small></h1>
         </div>
-      </div>
+
     </div>
   </header>
 
 <section id="main">
   <div class="container">
-    <div class="row">
+
 
       <section class="post-list">
 
+
+
+            <?php
+            require('php/DBHandler.php');
+            $db = new DBHandler();
+            $db->connect();
+            $posts = $db->getBlogPosts();
+            $db->closeConnection();
+            ?>
+
+            <?php foreach ($posts as $post) : ?>
+
+              <!-- generated post  -->
+              <div class="panel panel-default blogpost">
+                <div class="panel-heading">
+                  <h2 class="panel-title"><?php echo $post['title']; ?></h2>
+                </div>
+                <div class="panel-body">
+                  <small class="date-posted-box">Posted <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
+                  </br>
+                  <?php echo $post['body']; ?>
+                </div>
+              </div>
+
+            <?php endforeach; ?>
+
       </section>
 
-      <?php
-      require('php/DBHandler.php');
-      $db = new DBHandler();
-      $db->connect();
-      $posts = $db->getBlogPosts();
-      $db->closeConnection();
-      ?>
-
-      <?php foreach ($posts as $post) : ?>
-
-        <!-- generated post  -->
-        <div class="panel panel-default blogpost">
-          <div class="panel-heading">
-            <h2 class="panel-title"><?php echo $post['title']; ?></h2>
-          </div>
-          <div class="panel-body">
-            <small class="date-posted-box">Posted <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
-            </br>
-            <?php echo $post['body']; ?>
-          </div>
-        </div>
-
-      <?php endforeach; ?>
-
-    </div>
   </div>
 </section>
 
@@ -68,49 +69,6 @@
     <p>Nackademin</p>
     <p>2020-03-26</p>
   </footer>
-
-<!-- Modals -->
-
-<!-- Add Page -->
-<div class="modal fade" id="addPage" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <form>
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Add Page</h4>
-        </div>
-        <div class="modal-body">
-          <div class="form-group">
-            <label>Page Title</label>
-            <input type="text" class="form-control" placeholder="Page Title">
-          </div>
-          <div class="form-group">
-            <label>Page Body</label>
-            <textarea name="editor1" class="form-control" placeholder="Page Body"></textarea>
-          </div>
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Published
-            </label>
-          </div>
-          <div class="form-group">
-            <label>Meta Tags</label>
-            <input type="text" class="form-control" placeholder="Add Some Tags...">
-          </div>
-          <div class="form-group">
-            <label>Meta Description</label>
-            <input type="text" class="form-control" placeholder="Add Meta Description...">
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Save changes</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
 
 <script>
   CKEDITOR.replace('editor1');
