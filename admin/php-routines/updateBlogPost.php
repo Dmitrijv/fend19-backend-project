fbb6053ead5e173e9f36d480415e35387e70a3a7
Shan-Mi <shan.mi.fanfan@gmail.com>
2020-03-29 08:55:47 +0200

    added attatched_image property to post

--- admin/php-routines/updateBlogPost.php
+++ admin/php-routines/updateBlogPost.php
@@ -13,7 +13,7 @@
   $updatedBlogPost['media_iframe'] = $_POST["post-media_iframe"];
   $updatedBlogPost['published'] = UTILS::formCheckboxValueToBoolean($_POST["post-published"]);
   $updatedBlogPost['date_last_edit'] = date("Y-m-d H:i:s", time());
-
+  $updatedBlogPost['attatched_image'] ? $updatedBlogPost['attatched_image'] : null;
   CMS::updateBlogPost($updatedBlogPost);
 
   header("Location: ../index.php");
